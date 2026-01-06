836b20203d328d10729cd15830673557707f1e94
MarkAyman1 <markayman104@gmail.com>
2026-01-06 14:07:28 +0200

    make bloc to onboarding and remove social links

--- lib/core/api/services/cache_helper.dart
+++ lib/core/api/services/cache_helper.dart
@@ -45,4 +45,16 @@ class CacheHelper {
   static Future<void> clearAll() async {
     await _sharedPreferences.clear();
   }
+
+  // ================= Onboarding =================
+
+  static const String _onboardingKey = 'onboarding_completed';
+
+  static Future<void> saveOnboardingCompleted() async {
+    await _sharedPreferences.setBool(_onboardingKey, true);
+  }
+
+  static bool isOnboardingCompleted() {
+    return _sharedPreferences.getBool(_onboardingKey) ?? false;
+  }
 }
